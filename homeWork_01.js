b748e88cbda12e4040ae7e200cb7ca2ba69c489b
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2023-06-05 18:57:19 +0300

    add function deepEqual

--- homeWork_01.js
+++ homeWork_01.js
@@ -21,6 +21,27 @@ const makeCounter = () => {};
 
 //Бонус
 //Задание 1 – Написать функцию глубокого сравнения двух объектов
+// const obj1 = { here: { is:
+//   "on", other: "3" }, object: "Y" };
+
+//   const obj2 = { here: { is:
+//   "on", other: "2" }, object: "Y" };
+
+const deepEqual = (obj1, obj2) => {
+  let a = 0;
+  let b = 0;
+
+  for (let property in a) {
+    a += 1;
+  }
+  for (let property in b) {
+    b += 1;
+    if (!(property in a) || !deepEqual(obj1[property], obj2[property])) {
+      return false;
+    }
+  }
+  return a == b;
+};
 
 //Бонус
 //Задание 2 – Развернуть строку в обратном направлении при помощи методов массивов:
